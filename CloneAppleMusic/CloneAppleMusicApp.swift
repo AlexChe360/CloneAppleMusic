2c8c496453ba230e5550797c8ba94339e49cb730
Alexey <devalex360@gmail.com>
2024-06-18 10:11:36 +0500

    Initial Commit

---
+++ CloneAppleMusic/CloneAppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneAppleMusicApp.swift
+//  CloneAppleMusic
+//
+//  Created by  Alexey on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CloneAppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
